a7c08228e0d7f4c166efaaad24011f9ba06be75d
Justinas Grigas <34399460+SigSec@users.noreply.github.com>
2018-11-18 11:08:34 +0000

    wip

--- py/markerManager.py
+++ py/markerManager.py
@@ -15,7 +15,7 @@ class MarkerManager:
 		json_file.close()
 
 		# Add the new marker to the geojson file.
-		line_to_add = '{"event_type": "Feature","geometry": {"event_type": "Point","coordinates": [%s,%s]},"properties": ' + \
+		line_to_add = '{"type": "Feature","geometry": {"type": "Point","coordinates": [%s,%s]},"properties": ' + \
 			'{"title": "%s","icon": "%s"}},'
 		line_to_add = line_to_add % (str(_lat), str(_long), _title, _icon)
 		marker_array.insert(len(marker_array) - 1, line_to_add)
